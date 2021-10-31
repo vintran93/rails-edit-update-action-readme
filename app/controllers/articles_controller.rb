859d3e170fd17b5458ec546f1116dd57b28aa965
vincent <vincentuong@yahoo.com>
2021-10-31 00:58:27 -0500

    commit tree 6e6a8c16ce98556565336c2b9902df536e8e74ae

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+
   def index
     @articles = Article.all
   end
@@ -20,4 +21,13 @@ class ArticlesController < ApplicationController
   end
 
   # add edit and update methods here
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(title: params[:article][:title], description: params[:article][:description])
+    redirect_to article_path(@article)
+  end
 end
